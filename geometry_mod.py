04b3399b202bedc6940b756ddc73f38906d2c1ef
moeqaz <usamaqazi98@gmail.com>
2022-10-04 23:19:09 -0400

    homework

---
+++ geometry_mod.py
@@ -0,0 +1,12 @@
+import math
+
+def circle_area(radius):
+    area = math.pi * radius ** 2
+    return area
+
+def hypotenuse(s1, s2):
+    c = math.sqrt(s1**2 + s2**2)
+    return c
+
+# print(circle_area(7))
+# print(hypotenuse(3,7))
